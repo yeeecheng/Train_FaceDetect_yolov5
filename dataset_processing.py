ad7410dfdd1b953d33a6eab5f5996919841bc0ef
yeeecheng <yichengliao1003@gmail.com>
2023-01-04 18:16:39 +0800

    first upload

---
+++ dataset_processing.py
@@ -0,0 +1,47 @@
+import os
+import cv2
+
+class dataset_processing:
+    
+    def __init__(self,opt):
+        
+        self.tar_path = opt.save
+        
+        self.train_num = int(opt.num*opt.split)
+        self.cur_num = 1
+        print(f"total num :{opt.num} , train num :{self.train_num} , split percent : {opt.split}")
+        
+        if not os.path.isdir(self.tar_path):
+            os.mkdir(self.tar_path)
+            os.mkdir(os.path.join(self.tar_path,"./images"))
+            os.mkdir(os.path.join(self.tar_path,"./labels"))
+            os.mkdir(os.path.join(self.tar_path,"./images/train"))
+            os.mkdir(os.path.join(self.tar_path,"./images/val"))
+            os.mkdir(os.path.join(self.tar_path,"./labels/train"))
+            os.mkdir(os.path.join(self.tar_path,"./labels/val"))
+            
+    def save_to_dataset(self,img,area):
+        
+        print(f"processing {self.cur_num}")
+        img = cv2.resize(img,(160,160))
+        
+        where = "train"
+        if self.cur_num > self.train_num:
+            where = "val"
+            
+        dataset_image_path = os.path.join(self.tar_path,"./images",where,str(self.cur_num)+".png")
+        dataset_label_path = os.path.join(self.tar_path,"./labels",where,str(self.cur_num)+".txt")
+        
+        cv2.imwrite(dataset_image_path,img)
+        x_min , y_min ,x_max ,y_max = area[0] ,area[1] , area[2] ,area[3]
+        h , w  ,d =img.shape
+        
+        x_center = float(x_min+x_max)/2.0 * float(1.0/w)
+        y_center = float(y_min+y_max)/2.0 *float(1.0/h)
+        yolo_w = (x_max - x_min)* float(1.0/w)
+        yolo_h = (y_max - y_min)* float(1.0/h)
+        
+        with open(dataset_label_path,"w") as f:
+            f.write(f"0 {x_center} {y_center} {yolo_w} {yolo_h}\n")
+        
+        self.cur_num+=1
---
+++ train_face_detect.py
@@ -0,0 +1,78 @@
+from retinaface import RetinaFace 
+import os  
+import cv2
+from dataset_processing import *
+import argparse
+
+def process(opt):
+    
+    src_path =opt.train_dataset
+    training_img_num = opt.num
+   
+    
+    data_processing = dataset_processing(opt)
+    cnt = 0
+    model = RetinaFace
+    for r, d ,f in os.walk(src_path):
+        
+        for file in f :
+            if cnt  < opt.start:
+                cnt+=1
+                data_processing.cur_num=cnt
+                continue
+            
+            img_path = os.path.join(r,file)
+            faces = model.detect_faces(img_path)
+            
+            if len(faces) != 1:
+                continue
+            img = cv2.imread(img_path)
+            
+            area = faces["face_1"]["facial_area"]
+            
+            area[0]-=10
+            if area[0]  < 0 : 
+                area[0] = 0
+            area[1] -= 10
+            if area[1]  < 0 : 
+                area[1] = 0
+            area[2] += 10
+            if area[2]  >= img.shape[1] : 
+                area[2] = img.shape[1]-1
+            area[3] += 10
+            if area[3]  >= img.shape[0] : 
+                area[3] = img.shape[0]-1
+            
+            data_processing.save_to_dataset(img ,area)
+                
+            cnt+=1
+            if cnt >= training_img_num+opt.start:
+                break
+            
+def main(opt):
+    process(opt)
+
+def parse_opt(known =False):
+    
+    ROOT = os.getcwd()
+    print(ROOT)
+    parser =argparse.ArgumentParser()
+    parser.add_argument("--root",type = str ,default=ROOT)
+    parser.add_argument("--train_dataset",type = str,default=os.path.join(ROOT,'./dataset/img_align_celeba'))
+    parser.add_argument("--save",type = str,default=os.path.join(ROOT,"./dataset/face_detect_dataset"),help="the path where you save processing img")            
+    parser.add_argument("--start",type=int,default=0)
+    parser.add_argument("--num",type = int ,default=1500,help="the number which you want to train")
+    parser.add_argument("--split",type = float ,default=0.9,help="the percentage which you want split the dataset")
+    return parser.parse_known_args()[0] if known else parser.parse_args()
+
+def run(**kwargs):
+    
+    opt =parse_opt(True)
+    for k ,v in kwargs.items():
+        setattr(opt,k,v)
+    main(opt)
+    return opt
+
+
+if __name__ == "__main__":
+    run()
